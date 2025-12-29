aa2d0aae5e035f2ed78b039cc0cbb797cd0461e1
Pankaj <pka936636@gmail.com>
2025-12-29 13:55:37 +0530

    add backend arl

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,2 +1,3 @@
 
-export const BACKEND_URL="http://localhost:4001/api/v1";
+export const BACKEND_URL="https://e-commerce-app-backend-wa00.onrender.com";
+// export const BACKEND_URL="http://localhost:4001/api/v1";
